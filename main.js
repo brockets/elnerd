d2a02cf079af1371be9630f6ec588bb3cd73c93a
Marcin Ciarka <marcin@plamka.net>
2016-03-12 03:01:23 +0100

    ikona, poprawki styli

--- main.js
+++ main.js
@@ -12,7 +12,11 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: 'img/ikona.png'
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');

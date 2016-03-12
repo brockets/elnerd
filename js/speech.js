77d825ea472da0750d84f29725b3e7ad251d147b
Marcin Ciarka <marcin@plamka.net>
2016-03-12 05:00:48 +0100

    speech, style

--- js/speech.js
+++ js/speech.js
@@ -1,1 +1,23 @@
-// elo
+
+$(document).on('click', '#actionButton', function(){
+	$('.overlay').css('display', 'flex');
+    var msg = new SpeechSynthesisUtterance();
+    var voices = window.speechSynthesis.getVoices();
+    var commands = {
+        '*say': function (say) {
+            msg.text = 'Powiedziałeś: '+say;
+            msg.lang = 'pl';
+            window.speechSynthesis.speak(msg);
+        }
+    };
+    annyang.setLanguage('pl')
+    annyang.addCommands(commands);
+    annyang.debug();
+    annyang.start();
+
+    $('webview').css('transform', 'scale(1.2)')
+});
+
+$(document).on('click', '.overlay', function(){
+	$('.overlay').css('display', 'none');
+});

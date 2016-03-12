d001f94e30f878f44e7c7463090e706296d1cca2
Marcin Ciarka <marcin@plamka.net>
2016-03-12 09:58:27 +0100

    cokolwiek, chuj

--- js/speech.js
+++ js/speech.js
@@ -1,12 +1,41 @@
-var msg = new SpeechSynthesisUtterance();
+
+function recognizePage(strona) {
+	return 'http://facebook.pl';
+}
+
 var commands = {
-    '*say': function (say) {
-        msg.text = 'Powiedziałeś: '+say;
-        msg.lang = 'pl';
-        window.speechSynthesis.speak(msg);
-    }
+	'włącz *strona': function (strona) {
+		url = recognizePage(strona);
+		$('#speechContainer').text('Przejdź do ' + strona);
+		setTimeout(function(){
+			console.log(url);
+			if($('.micwrapper').is(':visible')) {
+				$('.micwrapper').hide();
+			}
+			if($('webview').is(':hidden')) {
+				$('webview').fadeIn(1, function(){
+        			document.querySelector('webview').src = url;
+				});
+			}
+		}, 1000);
+	},
+	'*say': function (say) {
+		$('#speechContainer').text(say);
+	},
 };
 annyang.setLanguage('pl')
 annyang.addCommands(commands);
 annyang.debug();
 annyang.start();
+
+$(document).on('click', '#actionButton', function(){
+	'use strict';
+	$(this).addClass('speechDisabled');
+	annyang.pause();
+});
+
+$(document).on('click', '#actionButton.speechDisabled', function(){
+	'use strict';
+	$(this).removeClass('speechDisabled');
+	annyang.resume();
+});
